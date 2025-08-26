665fc8fef91ca6d930b6f15128f50a856e11b2fe
Mateus Marcelino <mateusmarcelino.tds2.senai@gmail.com>
2025-08-26 11:04:19 -0300

    feat: create fuction dividir and multiplicar

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,4 +1,4 @@
- import { somar, subtrair  } from "../src/calculadora.js";
+ import { somar, subtrair, multiplicar, dividir   } from "../src/calculadora.js";
 
  describe("Calculadora", () => {
    test("Deve somar dois números corretamente", () => {
@@ -26,6 +26,36 @@
     // Assert (Verificar)
      expect(resultado).toBe(-1);
    });
+
+   test("Deve multiplicar dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+     const a = 2;
+     const b = 3;
+
+    // Act (Executar)
+    const resultado = multiplicar(a, b);
+
+    // Assert (Verificar)
+     expect(resultado).toBe(6);
+   });
+
+   test("Deve dividir dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+     const a = 10;
+     const b = 5;
+
+    // Act (Executar)
+    const resultado = dividir(a, b);
+
+    // Assert (Verificar)
+     expect(resultado).toBe(2);
+   });
+
+   test("Deve lançar um erro ao dividir por zero", () => {
+   expect(() => {
+    dividir(10, 0);
+   }).toThrow("Não é possível dividir por zero");
  });
-
-
+ });
--- src/calculadora.js
+++ src/calculadora.js
@@ -7,4 +7,13 @@ export function subtrair(a, b) {
   return a - b;
 }
 
+export function multiplicar(a, b) {
+  return a * b;
+}
 
+export function dividir(a, b) {
+  if (b == 0 ) {
+    throw new Error("Não é possível dividir por zero");
+  }
+  return a / b;
+}
